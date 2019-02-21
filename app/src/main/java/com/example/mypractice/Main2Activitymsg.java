b37f186a0901e01f995f5e064ad180bd4e68632d
venkster11 <venkateshm11799@gmail.com>
2019-02-21 22:12:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mypractice/Main2Activitymsg.java
@@ -0,0 +1,13 @@
+package com.example.mypractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activitymsg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2_activitymsg);
+    }
+}
